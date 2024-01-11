863ccc41f4c5aea4c80a5e97480cc07b63d84423
Unchained112 <51079412+Unchained112@users.noreply.github.com>
2024-01-11 22:44:22 +0100

    add attack collider area

--- scripts/Player/air_state.gd
+++ scripts/Player/air_state.gd
@@ -3,7 +3,7 @@ extends State
 @export var ground_state: State
 @export var air_attack_state: State
 @export var dash_scale: float = 2.0
-@export var air_scale: float = 0.8
+@export var air_scale: float = 0.2
 @export var double_jump_velocity: float = -420.0
 
 var can_double_jump: bool = true
@@ -11,18 +11,17 @@ var is_air_attack: bool = false
 var direction: float = 0.0;
 
 func on_state_enter():
-	# TODO: Fix jump after air attack
 	if not is_air_attack:
 		can_double_jump = true
 	is_air_attack = false
 
 func state_process(_delta):
 	direction = Input.get_axis("move_left", "move_right")
-	character.velocity.x = character.speed * direction * air_scale
+	character.velocity.x = character.speed * direction
 	# Air dash
 	if anime_state_machine.get_current_node() == "AirDash":
 		character.velocity.x *= dash_scale
-
+		character.velocity.y *= air_scale
 	if(character.is_on_floor()):
 		anime_state_machine.travel("Land")
 
@@ -51,5 +50,5 @@ func _on_animation_tree_animation_finished(anim_name):
 	if anim_name == "land":
 		next_state = ground_state
 	if anime_state_machine.get_current_node() == "AirDash":
-		character.velocity.y = 0.0
+		#character.velocity.y = 0.0
 		anime_state_machine.travel("Fall")
